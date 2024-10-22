bf9dbf4091b68cb461acce5d7f6f0821dbd1d501
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-10-22 11:04:30 +0200

    sketchy: remove config

--- sketchybar/items/cpu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item cpu right \
-	--set cpu update_freq=1 \
-	icon=􀫥 \
-	script="$PLUGIN_DIR/cpu.sh"
--- sketchybar/items/memory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item memory right \
-	--set memory update_freq=1 \
-	icon=􀫦 \
-	script="$PLUGIN_DIR/memory.sh"
