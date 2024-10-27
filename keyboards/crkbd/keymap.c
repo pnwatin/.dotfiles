63a12e700693cdbbabe5db086ca94cc79a37225e
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-10-27 20:50:13 +0100

    keyboard: merge numeric and symbols layouts into one with home row mods

--- keyboards/crkbd/keymap.c
+++ keyboards/crkbd/keymap.c
@@ -18,28 +18,77 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+// FIX: this resolve the issue with mith modifier keys and shifted keycodes (here # and $)
+// see: https://github.com/qmk/qmk_firmware/issues/50
+enum custom_keycodes {
+    CTL_HASH = SAFE_RANGE,
+    ALT_DLR,
+};
 
-// Left-hand home row mods
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint16_t tap_timer;
+    switch (keycode) {
+        case CTL_HASH:
+            if (record->event.pressed) {
+                tap_timer = timer_read();
+                register_code(KC_LCTL);
+            } else {
+                unregister_code(KC_LCTL);
+                if (timer_elapsed(tap_timer) < TAPPING_TERM) {
+                    tap_code16(LSFT(KC_3));
+                }
+            }
+            return false;
+        case ALT_DLR:
+            if (record->event.pressed) {
+                tap_timer = timer_read();
+                register_code(KC_LALT);
+            } else {
+                unregister_code(KC_LALT);
+                if (timer_elapsed(tap_timer) < TAPPING_TERM) {
+                    tap_code16(LSFT(KC_4));
+                }
+            }
+            return false;
+    }
+    return true;
+}
+
+// BASE layer home row mods
+// Left-hand
 #define CTL_A LCTL_T(KC_A)
 #define ALT_S LALT_T(KC_S)
 #define GUI_D LGUI_T(KC_D)
 #define SFT_F LSFT_T(KC_F)
 
-// Right-hand home row mods
+// Right-hand
 #define SFT_J RSFT_T(KC_J)
 #define GUI_K RGUI_T(KC_K)
 #define ALT_L LALT_T(KC_L)
 #define CTL_SCLN RCTL_T(KC_SCLN)
 
-#define RALT_SPC RALT_T(KC_SPC)
+// NUM layer home row mods
+// Left-hand
+// #define CTL_HASH MT(MOD_LCTL, KC_HASH)
+// #define ALT_DLR MT(MOD_LALT, KC_DLR)
+#define GUI_LCBR LGUI_T(KC_LCBR)
+#define SFT_RCBR LSFT_T(KC_RCBR)
 
-#define NAV LT(1, KC_TAB)
-#define NUM LT(2, KC_BSPC)
-#define SYM LT(3, KC_ENT)
-#define MEDIA LT(4, KC_ESC)
-#define FN LT(5, KC_ENT)
+// Right-hand
+#define SFT_4 RSFT_T(KC_4)
+#define GUI_5 RGUI_T(KC_5)
+#define ALT_6 LALT_T(KC_6)
+#define CTL_QUOT RCTL_T(KC_QUOT)
+
+#define RALT_DEL RALT_T(KC_SPC)
+
+#define NAV_ESC LT(1, KC_ESC)
+#define NUM_TAB LT(2, KC_TAB)
+#define MDI_SPC LT(3, KC_SPC)
+#define FN_ENT LT(4, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // BASE layer ✅
   [0] = LAYOUT_split_3x6_3(
    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
@@ -48,10 +97,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                             MEDIA,     NAV,RALT_SPC,         FN,     NUM,     SYM
+                                           NAV_ESC, NUM_TAB, MDI_SPC,     FN_ENT, KC_BSPC,RALT_DEL
                                        //`--------------------------'  `--------------------------'
    ),
 
+  // NAV layer ✅
   [1] = LAYOUT_split_3x6_3(
    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -60,36 +110,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           XXXXXXX, _______, XXXXXXX,     KC_ENT, KC_BSPC,  KC_DEL
+                                           _______, XXXXXXX, XXXXXXX,     KC_ENT, KC_BSPC,  KC_DEL
                                        //`--------------------------'  `--------------------------'
     ),
 
+  // NUM and SYM layer ✅
   [2] = LAYOUT_split_3x6_3(
    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       XXXXXXX, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, KC_EXLM,   KC_AT, KC_LPRN, KC_RPRN, KC_AMPR,                      KC_PLUS,    KC_7,    KC_8,    KC_9, KC_ASTR, XXXXXXX,
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       XXXXXXX, KC_QUOT,    KC_4,    KC_5,    KC_6,  KC_EQL,                      XXXXXXX, KC_RSFT, KC_RGUI, KC_LALT, KC_RCTL, XXXXXXX,
+       XXXXXXX,CTL_HASH, ALT_DLR,GUI_LCBR,SFT_RCBR,  KC_GRV,                       KC_EQL,   SFT_4,   GUI_5,   ALT_6,CTL_QUOT, XXXXXXX,
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       XXXXXXX,  KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX, KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_PIPE,                      KC_MINS,    KC_1,    KC_2,    KC_3, KC_BSLS, XXXXXXX,
    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_DOT,    KC_0, KC_MINS,    XXXXXXX, _______, XXXXXXX
+                                           XXXXXXX, _______, XXXXXXX,     KC_ENT, KC_UNDS,    KC_0
                                        //`--------------------------'  `--------------------------'
     ),
 
+
+  // MEDIA layer ✅
   [3] = LAYOUT_split_3x6_3(
-   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       XXXXXXX, KC_LCBR, KC_AMPR, KC_ASTR, KC_RPRN, KC_RCBR,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       XXXXXXX,  KC_DQT,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,                      XXXXXXX, KC_RSFT, KC_RGUI, KC_LALT, KC_RCTL, XXXXXXX,
-   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       XXXXXXX, KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_LPRN, KC_RPRN, KC_UNDS,    XXXXXXX, XXXXXXX, _______
-                                       //`--------------------------'  `--------------------------'
-    ),
-
-
-  [4] = LAYOUT_split_3x6_3(
    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -97,11 +137,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX,
    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           _______, XXXXXXX, XXXXXXX,    KC_MSTP, KC_MPLY, KC_MUTE
+                                           XXXXXXX, XXXXXXX, _______,    KC_MSTP, KC_MPLY, KC_MUTE
                                        //`--------------------------'  `--------------------------'
     ),
 
-  [5] = LAYOUT_split_3x6_3(
+  // FN layer ✅
+  [4] = LAYOUT_split_3x6_3(
    //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        XXXXXXX,  KC_F12,   KC_F7,   KC_F8,   KC_F9, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX,
    //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
